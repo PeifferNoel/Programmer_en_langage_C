d1c4cc439b4ce2f459271f2a7d47f8f732735e19
TheHawksur20 <n.peiffer@ludus-academie.com>
2019-10-14 17:03:04 +0200

    no message

--- Programmer_en_C_V1.0/main.c
+++ Programmer_en_C_V1.0/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NFOIS 5
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int i;
+    float x;
+    float racx;
+
+    printf("Bonjour \n");
+
+    printf("Je vais vous calculer %d racines carrees\n", NFOIS);
+
+    for(i=0;i<NFOIS;i++) {
+        printf("Donnez un nombre : ");
+        scanf("%f", &x);
+        if (x<0.0)
+            printf("Le nombre %f ne possède pas de racine carree \n", x);
+        else{
+            racx=sqrt(x);
+            printf("le nombre %f a pour racine carrée : %f\n",x,racx);
+        }
+    }
+
+    printf("C FINI A PLUS LES ZAMIS");
 }
